b296f3005fb826d66030f00f54e04a8a164e562e
ssmktr <ssmktr@naver.com>
2016-10-19 17:07:53 +0900

    no message

--- KTA/Assets/Scripts/Main.cs
+++ KTA/Assets/Scripts/Main.cs
@@ -3,9 +3,21 @@ using System.Collections;
 
 public class Main : MonoBehaviour {
 
+    public GameObject Btn1;
+    public UILabel Value1;
+
+    void Awake()
+    {
+        UIEventListener.Get(Btn1).onClick = (sender) =>
+        {
+            Value1.text = "Click";
+        };
+    }
+
 	void Start () {
-	
-	}
+        Value1.text = "Init";
+
+    }
 	
 	void Update () {
 	
